2420687580574e6ad11f9e93a2f83d8dfb91a98c
maksim05073 <maksimmixcer@gmail.com>
2025-05-29 08:52:31 +0300

    Add files via upload

---
+++ LB6_2/LB6_2/GeometricFigure.cs
@@ -0,0 +1,14 @@
+﻿namespace LB6_2;
+
+public abstract class GeometricFigure
+{
+    public string Name { get; set; }
+
+    public GeometricFigure(string name)
+    {
+        Name = name;
+    }
+    
+    public abstract double CalculateArea();
+    public abstract double CalculatePerimeter();
+}
---
+++ LB6_2/LB6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using LB6_2;
+
+class Program
+{
+    static void Main()
+    {
+        List<GeometricFigure> figures = new List<GeometricFigure>
+        {
+            new Circle(5) { Name = "Circle A" },
+            new Oval(3, 7) { Name = "Oval B" },
+            new Triangle(3, 4, 5) { Name = "Triangle C" },
+            new Circle(2) { Name = "Circle D" },
+            new Oval(5, 2) { Name = "Oval E" },
+            new Triangle(6, 8, 10) { Name = "Triangle F" }
+        };
+        Console.Write("Введите минимальную площадь: ");
+        if (double.TryParse(Console.ReadLine(), out double minArea))
+        {
+            var result = figures
+                .Where(f => f.CalculateArea() >= minArea)
+                .ToList();
+
+            Console.WriteLine($"\nФигуры с площадью не меньше {minArea}:");
+            foreach (var figure in result)
+            {
+                Console.WriteLine($"{figure.Name} ({figure.GetType().Name})");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Неверный ввод!");
+        }
+        Console.ReadKey();
+    }
+}
